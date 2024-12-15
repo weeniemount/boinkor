d55e8458c5823de907eeef102cca5ef5973fe81f
weeniemount <59868100+weeniemount@users.noreply.github.com>
2024-12-15 13:43:05 +0100

    boinkor certified

--- compile.bat
+++ compile.bat
@@ -6,4 +6,4 @@ del icon.o
 windres icon.rc -o icon.o
 cd ..
 cd ..
-gcc src/main.c src/resource/icon.o -lgdi32 -o boinkor.exe && boinkor.exe
+gcc src/main.c src/resource/icon.o -lgdi32 -luser32 -lcomdlg32 -o boinkor.exe && boinkor.exe
--- src/main.c
+++ src/main.c
@@ -1,6 +1,10 @@
 #include <windows.h>
+#include <commdlg.h>  // For file dialogs
+#include <stdio.h>    // For FILE structure and file handling
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+HWND hEdit;  // Declare hEdit globally
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     WNDCLASS wc = {0};
@@ -14,9 +18,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         return -1;
     }
 
-    // za menu
+    // Menu definitions
     #define EXIT 128
     #define ABOUT 256
+    #define SAVE 129
+    #define OPEN 130
     HMENU menu = CreateMenu();
     HMENU file = CreateMenu();
     HMENU help = CreateMenu();
@@ -24,11 +30,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     AppendMenu(menu, MF_POPUP, (UINT_PTR)file, "filez");
     AppendMenu(menu, MF_POPUP, (UINT_PTR)help, "hlep");
 
+    AppendMenu(file, MF_STRING, OPEN, "open");
+    AppendMenu(file, MF_STRING, SAVE, "save");
     AppendMenu(file, MF_STRING, EXIT, "exitearino");
-
     AppendMenu(help, MF_STRING, ABOUT, "abaut");
-
-
 
     // Create the window
     HWND hwnd = CreateWindowEx(
@@ -40,12 +45,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         NULL, menu, hInstance, NULL
     );
 
-    HWND hEdit = CreateWindowEx(
+    hEdit = CreateWindowEx(
         0,
         "EDIT",
         NULL,
-        WS_CHILD | WS_VISIBLE | WS_VSCROLL | WS_BORDER | ES_MULTILINE | ES_AUTOVSCROLL,
-        10, 10, 400, 200,
+        WS_CHILD | WS_VISIBLE | WS_VSCROLL | WS_BORDER | ES_MULTILINE | ES_AUTOVSCROLL | ES_WANTRETURN,
+        0, 0, 0, 0,
         hwnd, NULL, hInstance, NULL
     );
 
@@ -54,6 +59,25 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     }
 
     SetTimer(hwnd, 1, 1000, NULL);  // Timer ID 1, 1000 ms interval
+
+    // Set font for the edit control
+    HFONT hFont = CreateFont(
+        16,            // Height of font
+        0,             // Width of font
+        0,             // Angle of escapement
+        0,             // Orientation angle
+        FW_NORMAL,     // Weight
+        FALSE,         // Italic
+        FALSE,         // Underline
+        FALSE,         // StrikeOut
+        ANSI_CHARSET,  // Character set identifier
+        OUT_DEFAULT_PRECIS,  // Output precision
+        CLIP_DEFAULT_PRECIS,  // Clipping precision
+        DEFAULT_QUALITY,  // Quality
+        DEFAULT_PITCH | FF_DONTCARE,  // Pitch and family
+        "Courier New"  // Font name
+    );
+    SendMessage(hEdit, WM_SETFONT, (WPARAM)hFont, TRUE);
 
     // Show the window
     ShowWindow(hwnd, nCmdShow);
@@ -84,20 +108,85 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
                 case ABOUT:
                     MessageBox(hwnd, "the bestest textiest editorium of all time\n made by boink to boink", "abaut boinkor", MB_OK);
                     break;
+                case SAVE:
+                    {
+                        OPENFILENAME ofn;
+                        CHAR szFile[260] = {0};
+
+                        ZeroMemory(&ofn, sizeof(ofn));
+                        ofn.lStructSize = sizeof(ofn);
+                        ofn.hwndOwner = hwnd;
+                        ofn.lpstrFile = szFile;
+                        ofn.lpstrFile[0] = '\0';
+                        ofn.nMaxFile = sizeof(szFile);  // Correct field
+                        ofn.lpstrFilter = "Text Files\0*.txt\0";
+                        ofn.lpstrFileTitle = NULL;
+                        ofn.lpstrInitialDir = NULL;
+                        ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_OVERWRITEPROMPT;
+
+                        if (GetSaveFileName(&ofn)) {
+                            FILE *file = fopen(ofn.lpstrFile, "w");
+                            if (file) {
+                                int len = GetWindowTextLength(hEdit);
+                                CHAR *text = (CHAR *)GlobalAlloc(GPTR, (len + 1) * sizeof(CHAR));
+                                GetWindowText(hEdit, text, len + 1);
+                                fwrite(text, sizeof(CHAR), len, file);
+                                fclose(file);
+                                GlobalFree(text);
+                            }
+                        }
+                    }
+                    break;
+                case OPEN:
+                    {
+                        OPENFILENAME ofn;
+                        CHAR szFile[260] = {0};
+
+                        ZeroMemory(&ofn, sizeof(ofn));
+                        ofn.lStructSize = sizeof(ofn);
+                        ofn.hwndOwner = hwnd;
+                        ofn.lpstrFile = szFile;
+                        ofn.lpstrFile[0] = '\0';
+                        ofn.nMaxFile = sizeof(szFile);  // Correct field
+                        ofn.lpstrFilter = "Text Files\0*.txt\0";
+                        ofn.lpstrFileTitle = NULL;
+                        ofn.lpstrInitialDir = NULL;
+                        ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+
+                        if (GetOpenFileName(&ofn)) {
+                            FILE *file = fopen(ofn.lpstrFile, "r");
+                            if (file) {
+                                fseek(file, 0, SEEK_END);
+                                int len = ftell(file);
+                                fseek(file, 0, SEEK_SET);
+                                CHAR *text = (CHAR *)GlobalAlloc(GPTR, (len + 1) * sizeof(CHAR));
+                                fread(text, sizeof(CHAR), len, file);
+                                text[len] = '\0';
+                                SetWindowText(hEdit, text);
+                                fclose(file);
+                                GlobalFree(text);
+                            }
+                        }
+                    }
+                    break;
             }
             break;
         case WM_TIMER:
-            // Handle background refresh here (e.g., redrawing or updating content)
             InvalidateRect(hwnd, NULL, TRUE);
             break;
         case WM_PAINT:
             {
                 PAINTSTRUCT ps;
                 HDC hdc = BeginPaint(hwnd, &ps);
-                HBRUSH hBrush = CreateSolidBrush(RGB(173, 216, 230));  // Light blue color
+                HBRUSH hBrush = CreateSolidBrush(RGB(173, 216, 230));
                 FillRect(hdc, &ps.rcPaint, hBrush);
                 DeleteObject(hBrush);
                 EndPaint(hwnd, &ps);
+            }
+            break;
+        case WM_SIZE:
+            if (wParam != SIZE_MINIMIZED) {
+                MoveWindow(hEdit, 0, 0, LOWORD(lParam), HIWORD(lParam), TRUE);
             }
             break;
         default:
